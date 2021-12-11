35c919dba22400f1a3e4c7cbde779d263281ba8d
Caio Geraldes <caioaguida@protonmail.com>
2021-12-11 11:08:30 -0300

    pandas transform v1

--- src/lexicogenero/ferramentas/diorisis_reader.py
+++ src/lexicogenero/ferramentas/diorisis_reader.py
@@ -7,9 +7,14 @@ Github: https://github.com/caiogeraldes
 
 import os
 import json
-from typing import List,Dict,Any
+import pandas as pd
+import numpy as np
+from typing import List, Dict, Any, Union
+from betacode.conv import beta_to_uni
 
-def carrega_texto(nome_arquivo: str, diorisis_path: str) -> Dict[str,List[Any]]:
+
+def carrega_texto(nome_arquivo: str, diorisis_path: str) -> Dict[str,
+                                                                 List[Any]]:
     """docstring for carrega"""
     if diorisis_path[-1] != "/":
         diorisis_path = diorisis_path + "/"
@@ -24,7 +29,8 @@ def carrega_texto(nome_arquivo: str, diorisis_path: str) -> Dict[str,List[Any]]:
 
 def carrega_autor(autor: str, diorisis_path: str) -> Dict[str, List[Any]]:
     """ docstring for carrega_autor """
-    textos_autor = [x for x in os.listdir(diorisis_path) if x.startswith(autor)]
+    textos_autor = [x for x in os.listdir(diorisis_path)
+                    if x.startswith(autor)]
     corpus_autor: Dict[str, List[Any]] = dict()
 
     for texto in textos_autor:
@@ -33,10 +39,63 @@ def carrega_autor(autor: str, diorisis_path: str) -> Dict[str, List[Any]]:
     return corpus_autor
 
 
-def carrega_autores(autores: List[str], diorisis_path: str) -> Dict[str, List[Any]]:
+def carrega_autores(autores: List[str], diorisis_path: str) -> Dict[str,
+                                                                    List[Any]]:
     corpus: Dict[str, List[Any]] = dict()
 
     for autor in autores:
         corpus.update(carrega_autor(autor, diorisis_path))
 
     return corpus
+
+
+def em_plain_text(corpus: Dict[str, List[Any]], nome_arquivo: str) -> str:
+    sents = corpus[nome_arquivo]
+    plain_text: str = ""
+
+    for sent in sents:
+        for token in sent['tokens']:
+            plain_text += beta_to_uni(token['form']) + " "
+        plain_text += "\n"
+    return plain_text
+
+
+def em_pandas(corpus: Dict[str, List[Any]], nome_arquivo: str) -> pd.DataFrame:
+    sents = corpus[nome_arquivo]
+
+    data: List[Dict[str, Union[str, int, float]]] = list()
+
+    for sent in sents:
+        sent_id = sent['id']
+        sent_location = sent['location']
+        for token in sent['tokens']:
+            token_data: Dict[str, Union[str, int, float]] = dict()
+            token_data['sent_id'] = int(sent_id)
+            token_data['location'] = sent_location
+            if token['type'] == 'word':
+                token_data['form'] = beta_to_uni(token['form'])
+                if 'entry' in token['lemma'].keys():
+                    token_data['lemma'] = beta_to_uni(token['lemma']['entry'])
+                else:
+                    token_data['lemma'] = np.nan
+                if 'POS' in token['lemma'].keys():
+                    token_data['POS'] = token['lemma']['POS']
+                else:
+                    token_data['POS'] = np.nan
+                token_data['analyses'] = ";".join(token['lemma']['analyses'])
+                token_data['id'] = int(token['id'])
+            elif token['type'] == 'punct':
+                token_data['form'] = beta_to_uni(token['form'])
+                token_data['lemma'] = beta_to_uni(token['form'])
+                token_data['POS'] = 'punct'
+                token_data['analyses'] = 'punct'
+                token_data['id'] = 0
+            token_data['file'] = nome_arquivo
+
+            data.append(token_data)
+
+    df = pd.DataFrame(data)
+    df[['author', 'text']] = df['file'].str.split('-', 0, expand=True)
+    df['text'] = df['text'].str.replace(r'\([0-9]*\).json', '', regex=True).str.strip()
+    df['author'] = df['author'].str.replace(r'\([0-9]*\)', '', regex=True).str.strip()
+    return df
--- src/lexicogenero/main.py
+++ src/lexicogenero/main.py
@@ -7,10 +7,14 @@ from dotenv import load_dotenv
 # rompe runtime caso não esteja especificada.
 load_dotenv()
 DIORISIS_PATH = os.getenv("DIORISIS_PATH")
-assert DIORISIS_PATH != None, "Path para DIORISIS não especificada"
+assert DIORISIS_PATH is not None, "Path para DIORISIS não especificada"
 
 
 if __name__ == "__main__":
-    from ferramentas.diorisis_reader import carrega_autores#, carrega_autor, carrega_texto
+    from ferramentas.diorisis_reader import carrega_autores, em_plain_text, em_pandas
     print(DIORISIS_PATH)
-    print(carrega_autores(["Appian", "Herodotus"], DIORISIS_PATH).keys())
+    corpus = carrega_autores(["Herodotus"], DIORISIS_PATH)
+    HDT = 'Herodotus (0016) - Histories (001).json'
+    df = em_pandas(corpus, HDT)
+
+    print(df)
